e8898f4deff78ed17014a811b08176dfab800ebb
linomx90 <33378469+linomx90@users.noreply.github.com>
2026-09-26 13:34:54 -0400

    release-assets: JX General Dynamics 0.6.0rc16

--- observer/v5_solar_1pn/__init__.py
+++
@@ -1,6 +0,0 @@
-"""Isolated, nonauthorizing V5 Q2 perihelion observer candidate."""
-
-from . import candidate as _candidate
-from .candidate import *  # noqa: F401,F403
-
-__all__ = _candidate.__all__
--- oracle/v5_solar_1pn/__init__.py
+++
@@ -1,3 +0,0 @@
-"""Isolated, non-authorizing V5 Solar 1PN oracle candidate."""
-
-from .candidate import *  # noqa: F401,F403
